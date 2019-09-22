567fb3110c3b13ea944780f18c9195ef675cf30d
Brad Howes <bradhowes@mac.com>
2019-09-22 23:46:08 +0200

    Fixes for Xcode 11 and Swift 5.1

--- SmoothWalker.playground/Contents.swift
+++ SmoothWalker.playground/Contents.swift
@@ -5,40 +5,45 @@ _Jeff Biggus (@hyperjeff) March, 2016_
 import Cocoa
 import Accelerate
 
-var points: [CGPoint] = [CGPointMake(0, 0)]
+var points: [CGPoint] = [.zero]
 
 func randomNudge() -> CGPoint {
-	return CGPointMake(
-		CGFloat(Int(arc4random_uniform(101)) - 50) / 5.0,
-		CGFloat(Int(arc4random_uniform(101)) - 50) / 5.0)
+    return CGPoint(x: CGFloat(Int(arc4random_uniform(101)) - 50) / 5.0,
+                   y: CGFloat(Int(arc4random_uniform(101)) - 50) / 5.0)
 }
 
-infix operator + {}
-infix operator / {}
+// infix operator +
+// infix operator /
+
 func + (left: CGPoint, right:CGPoint) -> CGPoint {
-	return CGPointMake(left.x + right.x, left.y + right.y)
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 func / (left: CGPoint, right: Float) -> CGPoint {
-	return CGPointMake(left.x / CGFloat(right), left.y / CGFloat(right))
+    return CGPoint(x: left.x / CGFloat(right), y: left.y / CGFloat(right))
 }
 
-var v = CGPointMake(0, 0)
+var v = CGPoint.zero
 
 for _ in 0...99 {
 	let p = points.last!
 	v = randomNudge() + (v / 1.2)
-	points.append(CGPointMake(p.x + v.x, p.y + v.y))
+    points.append(CGPoint(x: p.x + v.x, y: p.y + v.y))
 }
 
 let path = NSBezierPath()
-path.appendBezierPathWithPoints(UnsafeMutablePointer<CGPoint>(points), count: points.count)
+points.withUnsafeBufferPointer { buffer in
+    buffer.baseAddress!.withMemoryRebound(to: NSPoint.self, capacity: points.count) { nsp in
+        path.appendPoints(NSPointArray(mutating: nsp), count: points.count)
+    }
+}
+
 path
 // tease out the x and y values
 let xs: [Float] = points.map { Float($0.x) }
 let ys: [Float] = points.map { Float($0.y) }
 
 // set up a variable to hold the result
-var distance = [Float](count: points.count, repeatedValue: 0)
+var distance = [Float](repeating: 0, count: points.count)
 
 // one-liner!
 vDSP_vdist(xs, 1, ys, 1, &distance, 1, vDSP_Length(points.count))
@@ -56,8 +61,8 @@ minimumX
 
 let windowSize: Int = 5
 
-var smoothXs = [Float](count: points.count, repeatedValue: 0)
-var smoothYs = [Float](count: points.count, repeatedValue: 0)
+var smoothXs = [Float](repeating: 0, count: points.count)
+var smoothYs = [Float](repeating: 0, count: points.count)
 let window = vDSP_Length(windowSize)
 
 vDSP_vswsum(xs, 1, &smoothXs, 1, vDSP_Length(points.count), window)
@@ -69,14 +74,17 @@ smoothXsGuts.map { $0 }
 
 var smoothPoints: [CGPoint] = []
 for i in 0 ..< smoothXs.count - windowSize - 1 {
-	let x = smoothXs[i] / 5
-	let y = smoothYs[i] / 5
-	smoothPoints.append(CGPointMake(CGFloat(x), CGFloat(y)))
+	let x = CGFloat(smoothXs[i] / 5)
+	let y = CGFloat(smoothYs[i] / 5)
+    smoothPoints.append(CGPoint(x: x, y: y))
 }
 
 let smoothPath = NSBezierPath()
-smoothPath.appendBezierPathWithPoints(
-	UnsafeMutablePointer<CGPoint>(smoothPoints), count: smoothPoints.count)
+smoothPoints.withUnsafeBufferPointer { buffer in
+    buffer.baseAddress!.withMemoryRebound(to: NSPoint.self, capacity: smoothPoints.count) { nsp in
+        smoothPath.appendPoints(NSPointArray(mutating: nsp), count: smoothPoints.count)
+    }
+}
 
 path
 smoothPath
